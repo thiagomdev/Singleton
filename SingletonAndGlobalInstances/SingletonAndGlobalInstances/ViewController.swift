48902f43a711fea5874644f3105efe5502cd552e
Thiago Monteiro <thiagomonteiro.developer@gmail.com>
2024-08-01 06:08:44 -0300

    feature(App):
    - First App's commit

---
+++ SingletonAndGlobalInstances/SingletonAndGlobalInstances/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingletonAndGlobalInstances
+//
+//  Created by Thiago Monteiro on 01/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
